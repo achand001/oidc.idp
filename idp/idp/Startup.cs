5202270638b4e7fd455d95a07f2dfaf312a33fa8
Anvil A Chand <achand@barfoot.co.nz>
2018-08-10 13:46:50 +1200

    added mvc core

--- idp/idp/Startup.cs
+++ idp/idp/Startup.cs
@@ -15,6 +15,13 @@ namespace idp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +32,10 @@ namespace idp
                 app.UseDeveloperExceptionPage();
             }
 
-           
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
         }
     }
 }
